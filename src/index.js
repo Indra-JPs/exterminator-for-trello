61f468c33461a748c5fa9f81abc5103b1b03e653
Goncalves Daniel <rgdaniel@indracompany.com>
2020-06-18 01:13:08 -0300

    #109 - test sending email using Mailjet's api

--- src/index.js
+++ src/index.js
@@ -106,3 +106,36 @@ prompt([
 
     await enviarEmail(mensagemFormatada, respostas.milestone);
 });
+
+
+const mailjet = require ('node-mailjet')
+.connect('', '')
+const request = mailjet
+.post("send", {'version': 'v3.1'})
+.request({
+  "Messages":[
+    {
+      "From": {
+        "Email": "rodrigodanielgoncalves@gmail.com",
+        "Name": "Rodrigo"
+      },
+      "To": [
+        {
+          "Email": "rodrigodanielgoncalves@gmail.com",
+          "Name": "Rodrigo"
+        }
+      ],
+      "Subject": "Greetings from Mailjet.",
+      "TextPart": "My first Mailjet email",
+      "HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+      "CustomID": "AppGettingStartedTest"
+    }
+  ]
+})
+request
+  .then((result) => {
+    console.log(result.body)
+  })
+  .catch((err) => {
+    console.log(err.statusCode)
+  })
